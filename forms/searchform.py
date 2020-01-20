c850db22fef225eb5277d581dfe3fafbf99c0cf5
JuliaKovalchook <uliakovalchuk@gmail.comheroku>
2020-01-20 23:36:22 +0300

    -

--- forms/searchform.py
+++ forms/searchform.py
@@ -5,7 +5,7 @@ from wtforms import validators
 
 class CreateQuery(FlaskForm):
 
-    taglist_checkOfCommand_List = StringField("taglist_checkOfCommand_List: ", [validators.DataRequired("Please enter your taglist_checkOfCommand_List.")])
+    nameOfCommand_List = StringField("nameOfCommand_List: ", [validators.DataRequired("Please enter your nameOfCommand_List.")])
     Expansion = StringField("Expansion: ", [validators.DataRequired("Please enter your Expansion.")])
     submit = SubmitField("Search")
 
